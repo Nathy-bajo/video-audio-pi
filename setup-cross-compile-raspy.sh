0ff54541024b886f47f524ab6101867661b8ac19
Nathy-bajo <bajon7680@gmail.com>
2022-10-21 13:32:39 +0100

    first commit

--- setup-cross-compile-raspy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-sudo apt-get install -qq gcc-aarch64-linux-gnu
-rustup target add aarch64-unknown-linux-gnu
-mkdir -p ~/.cargo
-cat >>~/.cargo/config <<EOF
-[target.aarch64-unknown-linux-gnu]
-linker = "aarch64-linux-gnu-gcc"
-EOF
